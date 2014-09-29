5357af936a11376c44fe53748a86d0c33d58a535
Ben Ubois <ben@benubois.com>
2014-09-28 20:04:41 -0700

    Moved options.

--- app/presenters/feed_presenter.rb
+++ app/presenters/feed_presenter.rb
@@ -3,7 +3,18 @@ class FeedPresenter < BasePresenter
   presents :feed
 
   def feed_link(&block)
-    @template.link_to @template.feed_entries_path(feed), remote: true, title: feed.title, class: 'feed-link', data: { behavior: 'selectable show_entries open_item feed_link countable', count_group: 'byFeed', count_group_id: feed.id, mark_read: {type: 'feed', data: feed.id, message: "Mark #{feed.title} as read?"}.to_json } do
+    options = {
+      remote: true,
+      title: feed.title,
+      class: 'feed-link',
+      data: {
+        behavior: 'selectable show_entries open_item feed_link countable',
+        count_group: 'byFeed',
+        count_group_id: feed.id,
+        mark_read: {type: 'feed', data: feed.id, message: "Mark #{feed.title} as read?"}.to_json
+      }
+    }
+    @template.link_to @template.feed_entries_path(feed), options do
       yield
     end
   end
